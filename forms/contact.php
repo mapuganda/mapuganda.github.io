105df4fd4b503524479203538db3640c54f795db
Ebong24 <67047118+Ebong24@users.noreply.github.com>
2022-05-19 15:56:17 +0300

    php updates failed

--- forms/contact.php
+++ forms/contact.php
@@ -1,9 +1,8 @@
 <?php
+  
+  $receiving_email_address = 'ronaldoyom@gmail.com';
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'info@mapuganda.org';
-
-  if( file_exists($php_email_form = '/assets/vendor/php-email-form/php-email-form.php' )) {
+  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
   } else {
     die( 'Unable to load the "PHP Email Form" Library!');
